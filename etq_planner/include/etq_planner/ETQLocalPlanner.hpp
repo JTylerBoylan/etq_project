f51bb3d186d5f1e3f60aa8e21e1f8625d9273404
jtylerboylan <jtylerboylan@outlook.com>
2022-07-20 18:02:27 -0400

    changes

---
+++ etq_planner/include/etq_planner/ETQLocalPlanner.hpp
@@ -0,0 +1,124 @@
+#ifndef ETQ_PLANNER_HPP
+#define ETQ_PLANNER_HPP
+
+#include <etq_planner/etq_definitions.hpp>
+
+#include <ros/ros.h>
+#include <grid_map_core/GridMap.hpp>
+#include <geometry_msgs/PoseArray.h>
+
+#include <thread>
+#include <vector>
+#include <queue>
+
+using namespace geometry_msgs;
+using namespace grid_map;
+
+namespace etq_planner
+{
+
+    class ETQLocalPlanner  {
+
+    public:
+
+        struct RunInfo {
+            bool path_found;
+            int n_iter;
+            int n_node;
+            float cost;
+        };
+
+        // Constructor
+        ETQLocalPlanner(ros::NodeHandle node_handle);
+
+        // Destructor
+        ~ETQLocalPlanner();
+
+        // Run path finding simulation
+        RunInfo run(const GridMap& grid, Pose start, Point goal);
+        
+        // Convert path from start to best node to pose array
+        void toPoseArray(geometry_msgs::PoseArray &arr_msg);
+
+        // Get all poses
+        void allPoses(geometry_msgs::PoseArray &arr_msg);
+
+    private:
+
+        // Struct to hold node information
+        struct Node {
+            float x, y, z;  // Position
+            float a, b, c;  // Orientation
+            float v, u;     // Velocity
+            float g, f;     // Heuristic
+            int i, p;       // Index
+            int t;          // Generation
+        };
+
+        int _max_iterations;
+        int _max_generations;
+        float _goal_radius;
+        float _sample_time;
+        float _sample_delta_time;
+        float _cost_time;
+        float _cost_delta_v;
+        float _cost_delta_u;
+        int _sample_size;
+        std::vector<float> _velocity_lookup;
+        std::vector<float> _rotation_lookup;
+
+        // Store the node buffer
+        // Max amount of nodes is (the max number of iterations) * (number of nodes per iteration) + (1 for start node)
+        Node * _buffer;
+
+        // Store grid map for planner
+        const GridMap * _grid;
+
+        // Store the start pose of the planner
+        Pose _start;
+
+        // Store the goal point of the planner
+        Point _goal;
+
+        // Store the index of the best node
+        int _best;
+
+        // Store the highest index
+        int _high;
+
+        // Store iteration count
+        int _iter;
+
+        // Generate sample path from current node
+        void _sample(const Node& _node, const int n, int& buf);
+
+        // Heuristic function for a node
+        float _h(const Node& node);
+
+        // F score of node
+        float _f(const Node& node);
+
+        // G score of node
+        float _g(const Node& node);
+
+        // Determine if node is in goal region
+        bool _in_goal_region(const Node& node);
+
+        // Determine if node is in valid position
+        bool _is_valid(const Node& node);
+
+        // Get velocity of node
+        float _vel(const Node& node, const int n);
+
+        // Get rotational velocity of node
+        float _rot(const Node& node, const int n);
+
+        // Convert node to pose
+        geometry_msgs::Pose _node2pose(const Node& node);
+
+
+    };
+
+}
+
+#endif
--- etq_planner/src/ETQLocalPlanner.cpp
+++ etq_planner/src/ETQLocalPlanner.cpp
@@ -1,4 +1,3 @@
-#include <ros/ros.h>
 #include <etq_planner/ETQLocalPlanner.hpp>
 #include <algorithm>
 #include <tf2/LinearMath/Quaternion.h>
@@ -13,10 +12,10 @@ namespace etq_planner
 	if (!node_handle.getParam("max_iterations", _max_iterations))
         	_max_iterations = DEFAULT_MAX_PLAN_ITERATIONS_LOCAL;
 
-	if (!node_handle.getParam("max_generation", _max_generation))
-        	_max_generation = DEFAULT_MAX_GENERATION_LOCAL;
+	if (!node_handle.getParam("max_generations", _max_generations))
+        	_max_generations = DEFAULT_MAX_GENERATIONS_LOCAL;
 
-	if (!node_handle.getParam("goal_radius", _goal_rad))
+	if (!node_handle.getParam("goal_radius", _goal_radius))
 		_goal_radius = DEFAULT_GOAL_RADIUS;
 
 	if (!node_handle.getParam("sample_time", _sample_time))
@@ -37,20 +36,18 @@ namespace etq_planner
 	if (!node_handle.getParam("sample_size", _sample_size))
 		_sample_size = DEFAULT_SAMPLE_SIZE;
 
-	if (!node_handle.getParam("sample_velocities", _vel_lookup))
-		_vel_lookup = DEFAULT_VELOCITY_LOOKUP_TABLE;
+	if (!node_handle.getParam("sample_velocities", _velocity_lookup))
+		_velocity_lookup = DEFAULT_VELOCITY_LOOKUP_TABLE;
 
-	if (!node_handle.getParam("sample_rotations", _rot_lookup))
-		_rot_lookup = DEFAULT_ROTATION_LOOKUP_TABLE;
+	if (!node_handle.getParam("sample_rotations", _rotation_lookup))
+		_rotation_lookup = DEFAULT_ROTATION_LOOKUP_TABLE;
 
-	if (_vel_lookup.size() != _sample_size || _rot_lookup.size() != _sample_size)
-		ROS_WARN("Mismatch of sample size and sample array size: \n
-			sample_size: %i\n
-			sample_velocities (size): %i\n
-			sample_rotations (size): %i", _sample_size, _vel_lookup.size(), _rot_lookup.size());
+	if (_velocity_lookup.size() != _sample_size || _rotation_lookup.size() != _sample_size)
+		ROS_WARN("Mismatch of sample size and sample array size:\nsample_size: %i\nsample_velocities (size): %i\nsample_rotations (size): %i", 
+            _sample_size, _velocity_lookup.size(), _rotation_lookup.size());
 
 	// Initialize Node buffer
-        _buffer = new Node[_max_iter*SAMPLE_SIZE + 1];
+        _buffer = new Node[_max_iterations*_sample_size + 1];
     }
 
     ETQLocalPlanner::~ETQLocalPlanner() { 
@@ -85,6 +82,7 @@ namespace etq_planner
         Node start_node;
         start_node.i = 0;
         start_node.p = -1;
+        start_node.t = 0;
         start_node.x = (float) start.position.x;
         start_node.y = (float) start.position.y;
         start_node.z = (float) start.position.z;
@@ -98,10 +96,14 @@ namespace etq_planner
 
         _buffer[0] = start_node;
 	
-	_best = 0;
+	    queue.push(0);
 
         // Iteration Loop
-        while (!queue.empty() && _iter < _max_iter) {
+        while (!queue.empty() && _iter < _max_iterations) {
+
+            // Get best node from open set
+            _best = queue.top();
+            queue.pop();
 
             Node node = _buffer[_best];
             
@@ -112,32 +114,27 @@ namespace etq_planner
             }
             
             //  Generation check
-            if (node.g >= _max_g)
+            if (node.t > _max_generations)
                 break;
 
             // Create arrays for threads and new buffer indices
-            std::thread threads[SAMPLE_SIZE];
+            std::thread threads[_sample_size];
 
-            int buf[SAMPLE_SIZE];
+            int buf[_sample_size];
 
             // Create sampling threads
-            for (int n = 0; n < SAMPLE_SIZE; n++)
-                threads[n] = std::thread(&ETQPlanner::_sample, this, std::ref(node), n, std::ref(buf[n]));
+            for (int n = 0; n < _sample_size; n++)
+                threads[n] = std::thread(&ETQLocalPlanner::_sample, this, std::ref(node), n, std::ref(buf[n]));
 
             // Close threads and add nodes to queue
-            for (int n = 0; n < SAMPLE_SIZE; n++) {
+            for (int n = 0; n < _sample_size; n++) {
                 threads[n].join();
                 if (buf[n])
                     queue.push(buf[n]);
             }
 
-            _high = (++_iter)*SAMPLE_SIZE;
+            _high = (++_iter)*_sample_size;
 
-	    // Get best node from open set
-            _best = queue.top();
-            queue.pop();
-
-            // End iteration
         }
 
         RunInfo info = {_path_found, _iter, _high, _buffer[_best].g};
@@ -148,16 +145,16 @@ namespace etq_planner
     }
     
     // Convert path from start to goal to pose array
-    void ETQPlanner::toPoseArray(geometry_msgs::PoseArray &arr_msg) {
+    void ETQLocalPlanner::toPoseArray(geometry_msgs::PoseArray &arr_msg) {
         // Back propogate
         for (int i = _best; i != -1; i = _buffer[i].p) {
             arr_msg.poses.push_back(_node2pose(_buffer[i]));
-            ROS_INFO("i: %d , z: %.2f, pitch: %.2f, roll: %.2f", i, _buffer[i].z, _buffer[i].b, _buffer[i].c);
+            //ROS_INFO("i: %d , z: %.2f, pitch: %.2f, roll: %.2f", i, _buffer[i].z, _buffer[i].b, _buffer[i].c);
         }
     }
 
     // Get all poses
-    void ETQPlanner::allPoses(geometry_msgs::PoseArray &arr_msg) {
+    void ETQLocalPlanner::allPoses(geometry_msgs::PoseArray &arr_msg) {
         for (int i = 0; i < _high; i++)
             if (_grid->isInside(Position(_buffer[i].x, _buffer[i].y)))
                 arr_msg.poses.push_back(_node2pose(_buffer[i]));
@@ -165,12 +162,12 @@ namespace etq_planner
 
 
     // Generate sample path from node
-    void ETQPlanner::_sample(const Node& _node, const int n, int& buf) {
+    void ETQLocalPlanner::_sample(const Node& _node, const int n, int& buf) {
 
         const float map_resolution = _grid->getResolution();
         
         // Calculate index in buffer
-        buf = _iter*SAMPLE_SIZE + n + 1;
+        buf = _iter*_sample_size + n + 1;
 
         // Make copy of node
         Node node = _node;
@@ -178,6 +175,7 @@ namespace etq_planner
         // Set new node values
         node.i = buf;
         node.p = _node.i;
+        node.t += 1;
 
         // Get velocities
         node.v = _vel(node, n);
@@ -185,17 +183,17 @@ namespace etq_planner
 
         // Sampling
         float t = 0;
-        while (t < SAMPLE_TIME) {
+        while (t < _sample_time) {
 
             // New Yaw
-            node.a += node.u * SAMPLE_DELTA_TIME;
+            node.a += node.u * _sample_delta_time;
             
             // Get heading direction
             Eigen::Vector2d heading(cos(node.a), sin(node.a));
             
             // New Position                   
-            node.x += heading.x() * node.v * SAMPLE_DELTA_TIME;
-            node.y += heading.y() * node.v * SAMPLE_DELTA_TIME;
+            node.x += heading.x() * node.v * _sample_delta_time;
+            node.y += heading.y() * node.v * _sample_delta_time;
 
             // Bounds check
             Position pos = Position(node.x, node.y);
@@ -227,7 +225,7 @@ namespace etq_planner
             }
 
             // Increment
-            t += SAMPLE_DELTA_TIME;
+            t += _sample_delta_time;
 
         }
         
@@ -247,7 +245,7 @@ namespace etq_planner
     }
 
     // Heuristic function for a pose
-    float ETQPlanner::_h(const Node& node) {
+    float ETQLocalPlanner::_h(const Node& node) {
         float dx, dy, dw;
         dx = _goal.x - node.x;
         dy = _goal.y - node.y;
@@ -259,44 +257,44 @@ namespace etq_planner
 
     // F score of a node
     // Returns: f score = distance from goal cost + time cost
-    float ETQPlanner::_f(const Node& node) {
+    float ETQLocalPlanner::_f(const Node& node) {
         return _h(node) + node.g;
     }
 
     // G score of node
-    float ETQPlanner::_g(const Node& node) {
-        return COST_TIME * SAMPLE_TIME 
-            + COST_DELTA_V * abs(node.v - _buffer[node.p].v) 
-            + COST_DELTA_U * abs(node.u - _buffer[node.p].u);
+    float ETQLocalPlanner::_g(const Node& node) {
+        // Fix this to include cost map
+        return _cost_time * _sample_delta_time 
+            + _cost_delta_v * abs(node.v - _buffer[node.p].v) 
+            + _cost_delta_u * abs(node.u - _buffer[node.p].u);
     }
 
     // Determine if node is in goal region
-    bool ETQPlanner::_in_goal_region(const Node& node) {
+    bool ETQLocalPlanner::_in_goal_region(const Node& node) {
         float dx = node.x - _goal.x;
         float dy = node.y - _goal.y;
-        return dx*dx + dy*dy <= GOAL_RADIUS_SQUARED;
+        return dx*dx + dy*dy <= _goal_radius * _goal_radius;
     }
 
     // Determine if node is in valid position
-    bool ETQPlanner::_is_valid(const Node& node) {
-        Position pos(node.x, node.y);
-        return abs(node.b) <= MAX_PITCH
-            && abs(node.c) <= MAX_ROLL;
+    bool ETQLocalPlanner::_is_valid(const Node& node) {
+        // Remove this?
+        return true;
     }
 
     // Get velocity of node
-    float ETQPlanner::_vel(const Node& node, const int n) {
+    float ETQLocalPlanner::_vel(const Node& node, const int n) {
         // return _vel_lookup[n] * (1 - node.b / MAX_PITCH) * (1 - node.c / MAX_ROLL);
-        return _vel_lookup[n];
+        return _velocity_lookup[n];
     }
 
     // Get rotational velocity of node
-    float ETQPlanner::_rot(const Node& node, const int n) {
-        return _rot_lookup[n];
+    float ETQLocalPlanner::_rot(const Node& node, const int n) {
+        return _rotation_lookup[n];
     }
     
     // Convert node to pose
-    geometry_msgs::Pose ETQPlanner::_node2pose(const Node& node) {
+    geometry_msgs::Pose ETQLocalPlanner::_node2pose(const Node& node) {
         geometry_msgs::Pose p;
         Position pos = {node.x, node.y};
         p.position.x = node.x;
